d473ee5c06375346d70c1340fdcb18812b2ce6fe
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-29 14:27:45 -0300

    Efetuado exercicio de conceituação e criação

---
+++ src/one/dio/basecamp/Main.java
@@ -0,0 +1,48 @@
+package one.dio.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        // int i;
+        int I;
+        // int 1a;
+        // int _1a;
+        // int $aq;
+
+        i = 5;
+        I = 10;
+        // _1a = 28;
+        // $aq = 7;
+
+        final int j = 10;
+        // j = 15;
+        int asrn24677md;
+        // int asrn24677 md;
+        // int asrn24677_md;
+        // int asrn24677%md;
+
+        asrn24677md = 100;
+        // asrn24677_md = 10;
+
+        int quantidadeProduto = 50;
+        // int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        // final int numeroTentativas = 5;
+        // int QUANTIDADE_OPCOES = 25;
+        // int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        // System.out.println(_1a);
+        // System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24677md);
+        // System.out.println(asrn24677_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        // System.out.println(QUANTIDADE_OPCOES);
+    }
+}

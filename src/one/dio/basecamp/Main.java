3647e80ca5521ca6b0049d2b1e19775fa69e6e39
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-30 00:07:33 -0300

    Efetuado exercicio de conversões(casting)

--- src/one/dio/basecamp/Main.java
+++ src/one/dio/basecamp/Main.java
@@ -2,98 +2,50 @@ package one.dio.basecamp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("PrePos");
-        prePos();
-        System.out.println("Aritmético");
-        aritmetico();
-        System.out.println("Atribuição");
-        atribuicao();
-        System.out.println("Precedência");
-        precedencia();
-    }
 
-    private static void prePos() {
-        int k = 10;
+        byte b1;
+        short s1 = 1000;
+        b1 = (byte) s1;
 
-        int i = ++k;
-        int j = k--;
-        int x = k;
+        long l1;
+        int i1 = 10;
+        l1 = i1;
 
-        System.out.println("i: " + i);
-        System.out.println("j: " + j);
-        System.out.println("x: " + x);
-    }
+        int i2;
+        long l2 = 1000000000000000000L;
+        i2 = (int) l2;
 
-    private static void aritmetico() {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
-        int e = 50;
+        int i3;
+        long l3 = 10000L;
+        i3 = (int) l3;
 
-        int r1 = a + b;
-        int r2 = c - b;
-        int r3 = d * b;
-        int r4 = e / a;
-        int r5 = c % b;
+        double  d1;
+        float f1 = 10.5F;
+        d1 = f1;
 
-        System.out.println("a + b: " + r1);
-        System.out.println("c - b: " + r2);
-        System.out.println("d * b: " + r3);
-        System.out.println("e / a: " + r4);
-        System.out.println("c % b: " + r5);
-    }
+        float f2;
+        float f3;
+        double d2 = 10000.58D;
+        f2 = (float) d2;
+        double d3 = 10000.58888888888888888888888888888888888888888888888D;
+        f3 = (float) d3;
 
-    private static void atribuicao() {
-        int i = 1500;
-        short j = 15;
-        long l = 500L;
-        int k = 35;
-        float f = 3.5F;
-        double d = f;
+        int i4;
+        float f4 = 11.5697F;
+        i4 = (int) f4;
 
-        System.out.println("d:" + d);
+        System.out.println("b1:" + b1 );
+        System.out.println("i1:" + i1 );
+        System.out.println("i2:" + i2 );
+        System.out.println("i3:" + i3 );
+        System.out.println("d1:" + d1 );
+        System.out.println("f2:" + f2 );
+        System.out.println("f3:" + f3 );
+        System.out.println("i4:" + i4 );
 
-        i += 5; // i = i + 5
-        j -= 3; // j = j -3
-        d /= 2.7d; //  d = d / 2.7d
-        l *= 3; // l = l * 3
-        k %= 2; // k = k % 2
+        b1 = (byte) d3;
 
-        System.out.println("i:" + i);
-        System.out.println("j:" + j);
-        System.out.println("d:" + d);
-        System.out.println("l:" + l);
-        System.out.println("k:" + k);
-
-        i = k = j;
-
-        System.out.println("k:" + k);
-        System.out.println("i:" + i);
-    }
-
-    private static void precedencia() {
-        int i = 10;
-        int j = 20;
-        int k = 30;
-
-        int a = i++ + --j * k; // 10 + 19 * 30 -> 10 + 570 -> 580
-
-        System.out.println("i++ + --j * k:" + a);
-
-        System.out.println("i: " + i); // 11
-
-        int b = k / --i % 3 + 1; // 30 / 10 % 3 + 1 -> 1
-
-        System.out.println("k / --i % 3 + 1:" + b);
-
-        System.out.println("i: " + i); // 10
-
-        int  c = 2;
-
-        c *=  i += 5; // c = 2 * i; i = i  +  5 -> c = 2 * i; i = 10 + 5; -> c = 2 * 15; ->  c = 2 * 15; c = 30
-
-        System.out.println("c *= i += 5: " + c);
+        System.out.println("b1:" + b1);
     }
 
 }
